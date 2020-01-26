8b31b8cda758565057d222694ebbbf0fff001cf0
damian_zalewskipl@o2.pl <damian_zalewskipl@o2.pl>
2020-01-26 15:15:47 +0100

    added cart, form adn policies

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     let itemsCount = $(".categoryItem").length;
 
     $(".categoryItem").hide();
-    
+    $(".categoryItem").first().show();
+
     for(let i = 1; i<=itemsCount; i++){
         $("#categoryBtn"+i).click(function(){
         $(".categoryItem").hide();
