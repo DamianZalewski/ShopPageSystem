48bac19c979100fc773b771de0cda7693c3deb19
damian_zalewskipl@o2.pl <damian_zalewskipl@o2.pl>
2020-02-01 13:00:33 +0100

    fixed empty inputs forms

--- form.js
+++ form.js
@@ -1,13 +1,41 @@
 $(document).ready(function(){
-        $("#sendForm").on("click", function(){
-            var d = new Date();
-            let formObj = {
-                    name : $("#firstNameInput").val(),
-                    surname : $("#lastNameInput").val(),
-                    date : d.toDateString(),
-                    email : $("#emailInput").val(),
-                    description: $("#descriptionInput").val() 
-                };
-            localStorage.setItem("form", JSON.stringify(formObj));
+        $("#sendForm").on("click", function(event){
+            let nameVal = $("#firstNameInput").val();
+            let surnameVal = $("#lastNameInput").val();
+            let emailVal = $("#emailInput").val();
+            let descriptionVal = $("#descriptionInput").val();
+                
+            $("#firstNameInput").removeClass("inputError");
+            $("#lastNameInput").removeClass("inputError");
+            $("#emailInput").removeClass("inputError");
+
+            
+            if(nameVal == "") {
+                $("#firstNameInput").addClass("inputError");
+            }
+            
+            if(surnameVal == "") {
+                $("#lastNameInput").addClass("inputError");
+            }
+            
+            if(emailVal == "") {
+                $("#emailInput").addClass("inputError");
+            }
+            
+            
+            if(nameVal == "" || surnameVal == "" || emailVal == ""){
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            
+//            var d = new Date();
+//            let formObj = {
+//                    name : nameVal,
+//                    surname : surnameVal,
+//                    date : d.toDateString(),
+//                    email : emailVal,
+//                    description: descriptionVal  
+//                };
+//            localStorage.setItem("form", JSON.stringify(formObj));
         });
 });
